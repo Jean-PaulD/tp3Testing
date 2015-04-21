bbfe2535d2bd8a0c14690f6adc1a7a95056b14d5
211064084 <211064084@CPT-EN-111B02.ADS.CPUT.AC.ZA>
2015-04-21 18:24:50 +0200

    18:23 4/04/2015

---
+++ src/main/java/com/mycompany/tp3testing/App.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.tp3testing;
+
+/**
+ *
+ * @author 211064084
+ */
+public class App {
+    
+}
---
+++ src/test/java/NewJUnitTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ *
+ * @author 211064084
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(class = App.class)//App is class name
+@WebAppConfiguration
+        
+public class NewJUnitTest {
+    @Autowired
+    LecturerRepository repository;
+    @Test
+    public void testCreate(){
+    Lecturer lect = new Lecturer.Builder("Lect 1");
+    
+    respository.save(lect);
+    }
+    
+    public NewJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
